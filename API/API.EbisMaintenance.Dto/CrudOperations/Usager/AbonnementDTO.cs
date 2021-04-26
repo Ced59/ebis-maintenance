03b5e32d79cf72f379c30e62f3ca91fcdf1424e8
Ambroise Linquette <ambroise.linquette@ltpdampierre.fr>
2021-04-26 16:11:18 +0200

    ajout Usager

---
+++ API/API.EbisMaintenance.Dto/CrudOperations/Usager/AbonnementDTO.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.EbisMaintenance.Dto.CrudOperations.Usager
+{
+    class AbonnementDTO
+    {
+
+        [JsonProperty(PropertyName = "dateDebut")]
+        public DateTime DateDebut { get; set; }
+
+        [JsonProperty(PropertyName = "dateDebut")]
+        public DateTime DateFin { get; set; }
+    }
+}
---
+++ API/API.EbisMaintenance.Entities/CrudOperations/Usager/Usager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.EbisMaintenance.Entities.CrudOperations.Usager
+{
+    class Usager
+    {
+        public string Nom { get; set; }
+
+        public string Prenom { get; set; }
+
+        public string Adresse { get; set; }
+
+        public string Ville { get; set; }
+
+        public string TelFixe { get; set; }
+
+        public string TelMobile { get; set; }
+
+        public Contrat Contrat { get; set; }
+
+    }
+}
