918afefdbfb801d66d6a89b626f4d0665751bcba
Cedric <c.caudron59@gmail.com>
2021-04-26 12:24:20 +0200

    Add Swagger

--- API/API.EbisMaintenance.WebAPI/Controllers/WeatherForecastController.cs
+++ API/API.EbisMaintenance.WebAPI/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace API.EbisMaintenance.WebAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/wheatherforecast")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
