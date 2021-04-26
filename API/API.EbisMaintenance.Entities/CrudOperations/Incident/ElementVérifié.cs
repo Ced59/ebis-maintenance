af94933255bed5f36021337e1e8341b5e3ee7606
Ambroise Linquette <ambroise.linquette@ltpdampierre.fr>
2021-04-26 16:44:47 +0200

    ajout incident

---
+++ API/API.EbisMaintenance.Entities/CrudOperations/Incident/ElementVérifié.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.EbisMaintenance.Entities.CrudOperations.Incident
+{
+    class ElementVérifié
+    {
+        public string Libelle { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
