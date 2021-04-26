678087e13b82e6845093c6ea71ec66e5009f76fc
Ambroise Linquette <ambroise.linquette@ltpdampierre.fr>
2021-04-26 16:29:28 +0200

    ajout OperationRecharge

---
+++ API/API.EbisMaintenance.Dto/CrudOperations/OperationRecharge/ControleDTO.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.EbisMaintenance.Dto.CrudOperations.OperationRecharge
+{
+    class ControleDTO
+    {
+
+        [JsonProperty(PropertyName = "libelle")]
+        public string Libelle { get; set; }
+
+        [JsonProperty(PropertyName = "détails")]
+        public string Détails { get; set; }
+    }
+}
