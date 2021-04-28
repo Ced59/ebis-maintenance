4f7344be63e3b697c58f3ac5e1ed7e30fa6296cc
Vincent Brocheton <vinc.brocheton@gmail.com>
2021-04-28 15:03:51 +0200

    View Details Incidents

--- WPF.Sample.ViewModelLayer/ListeIncidentViewModel.cs
+++ WPF.Sample.ViewModelLayer/ListeIncidentViewModel.cs
@@ -1,12 +1,17 @@
 ﻿using Common.Classes;
 using System.Collections.Generic;
+using WPF.MonAppli.CoucheDonnees.Entities.BorneEntities;
 using WPF.MonAppli.CoucheDonnees.Entities.IncidentEntitie;
+using WPF.MonAppli.CoucheDonnees.Entities.OperationRechargeEntities;
 using WPF.MonAppli.CoucheDonnees.Models;
 
 namespace WPF.MonAppli.CoucheViewModel
 {
     public class ListeIncidentViewModel : ViewModelBase
     {
+
+        private Incident incident;
+
         private List<Incident> allIncidents;
         public List<Incident> AllIncidents {
             get { return allIncidents; }
@@ -26,6 +31,16 @@ namespace WPF.MonAppli.CoucheViewModel
             var requestIncidents = new GetAllIncidents();
 
             AllIncidents = requestIncidents.LaunchRequest();
+        }
+
+        public Incident Incident
+        {
+            get { return incident; }
+            set
+            {
+                incident = value;
+                RaisePropertyChanged("Incident");
+            }
         }
     }
 }
