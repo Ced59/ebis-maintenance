2025a8559c251c803f41cac99a66a6353b275894
Ambroise Linquette <ambroise.linquette@ltpdampierre.fr>
2021-04-28 11:53:47 +0200

    Entretien

--- WPF.Sample/UserControls/EntretienControl.xaml.cs
+++ WPF.Sample/UserControls/EntretienControl.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Controls;
+using WPF.MonAppli.CoucheDonnees.Entities.IncidentEntitie;
 using WPF.MonAppli.CoucheViewModel;
 
 namespace WPF.MonAppli.UserControls
@@ -10,12 +12,16 @@ namespace WPF.MonAppli.UserControls
     public partial class EntretienControl : UserControl
     {
         private EntretienViewModel viewModel;
+        public List<Incident> AllIncident { get; set; }
+
 
         public EntretienControl()
         {
             InitializeComponent();
             viewModel = (EntretienViewModel)this.Resources["viewModel"];
+            viewModel.GetEntretiens();
         }
+
 
         private void FermerBouton_Click(object sender, RoutedEventArgs e)
         {
