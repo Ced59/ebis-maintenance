17be73b6efba4ee0e5be4f54ec35494cedf12da9
Vincent Brocheton <vinc.brocheton@gmail.com>
2021-04-29 09:49:17 +0200

    Add Statistic WPF

---
+++ WPF.Sample.DataLayer/Models/Statistics/TopElementsDefectueuxStatistics.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.MonAppli.CoucheDonnees.Models.Statistics
+{
+    class TopElementsDefectueuxStatistics
+    {
+        public RestClient Client { get; set; }
+        public RestRequest Request { get; set; }
+
+        public TopElementsDefectueuxStatistics(int deltaYear)
+        {
+            Client = new RestClient("https://localhost:44360");
+            Request = new RestRequest("api/incidents/top-five-defective-elements", Method.GET)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            Request.AddHeader("Content-Type", "application/json");
+        }
+
+        public ElementsDefectueuxAverage LaunchRequest()
+        {
+            var response = Client.ExecuteAsync(Request).GetAwaiter().GetResult();
+
+            return JsonConvert.DeserializeObject<ElementDefectueuxAverage>(response.Content);
+        }
+    }
+}
---
+++ WPF.Sample.DataLayer/Models/Statistics/TopElementsFiablesStatistics.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.MonAppli.CoucheDonnees.Models.Statistics
+{
+    class TopElementsFiablesStatistics
+    {
+        public RestClient Client { get; set; }
+        public RestRequest Request { get; set; }
+
+        public TopElementsFiablesStatistics(int deltaYear)
+        {
+            Client = new RestClient("https://localhost:44360");
+            Request = new RestRequest("api/incidents/top-five-reliable-elements", Method.GET)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            Request.AddHeader("Content-Type", "application/json");
+        }
+
+        public ElementsFiableAverage LaunchRequest()
+        {
+            var response = Client.ExecuteAsync(Request).GetAwaiter().GetResult();
+
+            return JsonConvert.DeserializeObject<ElementFiableAverage>(response.Content);
+        }
+    }
+}
