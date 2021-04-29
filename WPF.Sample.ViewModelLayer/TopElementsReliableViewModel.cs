a3a456947f2680c7a53d9963a20ce76cb20f65c9
Vincent Brocheton <vinc.brocheton@gmail.com>
2021-04-29 14:06:03 +0200

    Top make

--- WPF.Sample.ViewModelLayer/TopElementsReliableViewModel.cs
+++ WPF.Sample.ViewModelLayer/TopElementsReliableViewModel.cs
@@ -1,12 +1,32 @@
 ﻿using Common.Classes;
+using System.Collections.Generic;
+using WPF.MonAppli.CoucheDonnees.Entities;
+using WPF.MonAppli.CoucheDonnees.Models.Statistics;
 
 namespace WPF.MonAppli.CoucheViewModel
 {
     public class TopElementsReliableViewModel : ViewModelBase
     {
+        private List<StatElementDefectueux> topFiveReliableElements;
         public TopElementsReliableViewModel()
         {
             AfficherMessageStatut("Top 5 des bornes fiables");
+        }
+
+        public List<StatElementDefectueux> TopFiveReliableElements
+        {
+            get { return topFiveReliableElements; }
+            set
+            {
+                topFiveReliableElements = value;
+                RaisePropertyChanged("TopFiveReliableElements");
+            }
+        }
+
+        public void getElementReliable()
+        {
+            var request = new TopElementsFiablesStatistics();
+            TopFiveReliableElements = request.LaunchRequest();
         }
     }
 }
